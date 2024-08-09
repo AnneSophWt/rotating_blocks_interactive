0ed293aca19f8c91ef4743a7ff42e5b0ca0af5c6
Anne <anne-sophiewoite@AnneSophiesAir.fritz.box>
2024-08-09 10:21:44 +0200

    changes

---
+++ interactive_funny_blocks/public/block.js
@@ -0,0 +1,35 @@
+class Block {
+    constructor(x, y) {
+         this.x = x; 
+         this.y = y;
+        this.angle = 0;
+    }
+    display() {
+        noFill();
+        stroke(255);
+        push();
+        translate(this.x, this.y);
+        rotate(this.angle);
+        rect(0, 0, size, size);
+        pop();
+    }
+
+    move() {
+        // If the mouse is moving, check distance between mouse lox and center of sqaure
+        let distance;
+        if (pmouseX - mouseX != 0 || pmouseY - mouseY !=0) {
+            distance = dist(mouseX, mouseY, 
+                this.x, this.y);
+                if (distance < distMouse) {
+                this.angle += 1;
+        }
+    
+        // if squares are already rotating, keep rotating until angle = 90
+            if (this.angle > 0 && this.angle < 90) {
+                this.angle += 1; 
+        } else {
+            this.angle = 0; 
+        }
+    }
+    }
+}
